026061b92772198a543e4c9b67141736920ba739
MinorityKilla <pavelzel1007@gmail.com>
2025-04-27 19:58:16 +0300

    Add files via upload

---
+++ Money_Remaining/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        System.out.println("Итого: " + (income - spending) + " Тыс.");
+    }
+}
